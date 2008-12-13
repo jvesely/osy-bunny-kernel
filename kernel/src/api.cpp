d5cc8d453a9ceb05591d504c12bcc5ad84db9be8
Jan Vesely <jano.vesely@gmail.com>
2008-12-13 21:48:09 +0000

    va_macros moved
    git-svn-id: svn://aiya.ms.mff.cuni.cz/osy0809-depeslve/branches/vesely@406 19aa93f8-1956-4de8-b6a1-9b0d3797f1ac

--- kernel/src/api.cpp
+++ kernel/src/api.cpp
@@ -44,11 +44,6 @@
 #include "mem/FrameAllocator.h"
 
 
-#define va_start __builtin_va_start
-#define va_end __builtin_va_end
-#define va_arg __builtin_va_arg
-
-
 /*----------------------------------------------------------------------------*/
 size_t putc(const char c)
 {
